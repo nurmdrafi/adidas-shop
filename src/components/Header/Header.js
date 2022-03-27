0f3ba77fd476e569864a79223e24ad4561e43d8f
nurmdrafi <nurmdrafi2014@gmail.com>
2022-03-27 18:57:00 +0600

    header component added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Container, Navbar, Nav } from "react-bootstrap";
+import { AiOutlineShopping } from "react-icons/ai";
+
+const Header = () => {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="m-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#products">Products</Nav.Link>
+            <Nav.Link href="#about-us">About Us</Nav.Link>
+          </Nav>
+          <div>
+            <AiOutlineShopping
+              style={{ color: "white", fontSize: "25px" }}
+            ></AiOutlineShopping>
+          </div>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default Header;
